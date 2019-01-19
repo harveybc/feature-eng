60cd4858b1fc1668783f0ec0451da64b7b11394b
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-18 22:21:52 -0500

    Update q_datagen_v6.py

--- q_datagen_v6.py
+++ q_datagen_v6.py
@@ -232,7 +232,10 @@ if __name__ == '__main__':
     for i in range(1, num_ticks):        
         for j in range(0, num_columns):
             # asigna valor en matrix para valores retornados
-            my_data_r[i,j] = (my_data[i,j] - my_data[i-1,j]) / my_data[i-1,j]
+            if my_data[i-1,j] != 0:
+                my_data_r[i,j] = (my_data[i,j] - my_data[i-1,j]) / my_data[i-1,j]
+            else:
+                my_data_r[i,j] = (my_data[i,j] - my_data[i-1,j] + 1) / (my_data[i-1,j]+1)
             # actualiza max y min
             if my_data[i, j] > max[j]:
                 max[j] = my_data[i, j]
