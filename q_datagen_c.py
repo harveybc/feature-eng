fb5c2b23eac9c0f17e72f284394eec78c50c68ad
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-22 16:35:21 -0500

    Update q_datagen_c.py

--- q_datagen_c.py
+++ q_datagen_c.py
@@ -494,32 +494,12 @@ if __name__ == '__main__':
     # We use the default selection function: the 10=0.55,20=0.54, 25=0.51 30=0.52, 50=0.53  most significant features
     selector = SelectPercentile(mutual_info_classif, percentile=25)
     selector.fit(X, y)
-    scores = -np.log10(selector.scores_)
+    scores = selector.scores_
     scores /= scores.max()
-    plt.bar(X_indices - .45, scores, width=.2,
-            label=r'Univariate score ($-Log(p_{value})$)', color='darkorange',
+    plt.bar(X_indices, scores, width=.2,
+            label=r'Mutual information ', color='c',
             edgecolor='black')
-
-    # #############################################################################
-    # Compare to the weights of an SVM
-    clf = svm.SVC(kernel='linear')
-    
-    clf.fit(X, y)
-
-    svm_weights = (clf.coef_ ** 2).sum(axis=0)
-    svm_weights /= svm_weights.max()
-
-    plt.bar(X_indices - .25, svm_weights, width=.2, label='SVM weight',
-            color='navy', edgecolor='black')
-
-    clf_selected = svm.SVC(kernel='linear')
-    clf_selected.fit(selector.transform(X), y)
-    svm_weights_selected = (clf_selected.coef_ ** 2).sum(axis=0)
-    svm_weights_selected /= svm_weights_selected.max()
-    plt.bar(X_indices[selector.get_support()] - .05, svm_weights_selected,
-            width=.2, label='SVM weights after selection', color='c',
-            edgecolor='black')
-    plt.title("Comparing feature selection")
+    plt.title("Feature Selection")
     plt.xlabel('Feature number')
     plt.yticks(())
     plt.axis('tight')
