460072cb4869232764e47265ac50c6a7d03802bd
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-22 20:17:50 -0500

    Update q_datagen_c.py

--- q_datagen_c.py
+++ q_datagen_c.py
@@ -299,7 +299,7 @@ def get_reward(action, window, min_TP, max_TP, min_SL, max_SL, min_dInv, max_dIn
             return {'reward':0, 'profit':profit_sell, 'dd':dd_sell ,'min':min ,'max':max, 'direction':0}
     if action == 16:
         # RETURN DE MACD signal no normalizado (EMAf-EMAini) ADELANTADO 15 dias (TODO: Probar con period =7 y no 14 como el actual dataset)
-        if (window[3][7] - window[2][7]) > 0:
+        if (window[15][9] - window[14][9]) > 0:
             rew = 1
         else:
             rew = 0
