ea9dc3b531a608b66128f0a54567dca57d60515c
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-24 12:44:50 -0500

    test

--- q_datagen_c.py
+++ q_datagen_c.py
@@ -299,7 +299,7 @@ def get_reward(action, window, min_TP, max_TP, min_SL, max_SL, min_dInv, max_dIn
             return {'reward':0, 'profit':profit_sell, 'dd':dd_sell ,'min':min ,'max':max, 'direction':0}
     if action == 16:
         # RETURN DE MACD ADELANTADO 8 dias (TODO: Probar con period =7 y no 14 como el actual dataset)
-        if (window[9][9] - window[8][9]) > 0:
+        if (window[11][9] - window[10][9]) > 0:
             rew = 1
         else:
             rew = 0
