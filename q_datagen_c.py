dbe304e6c4fa9ac22b578008167ee5b8f82432d9
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-27 15:57:28 -0500

    Update q_datagen_c.py

--- q_datagen_c.py
+++ q_datagen_c.py
@@ -472,11 +472,11 @@ if __name__ == '__main__':
     # probando con min-max antes de prowertransform en las 3 últimas features
     # antes : eva=0.1 
     # con min-max : eva= TODO
-    from sklearn.preprocessing import MinMaxScaler
-    sc = MinMaxScaler(feature_range = (0, 1))
-    training_set_scaled = sc.fit_transform(to_tn)
+    #from sklearn.preprocessing import MinMaxScaler
+    #sc = MinMaxScaler(feature_range = (0, 1))
+    #training_set_scaled = sc.fit_transform(to_tn)
     
-    output_bt = pt.fit_transform(training_set_scaled)
+    output_bt = pt.fit_transform(to_tn)
     output_bc = concatenate((output_bt,to_t[: , ((2 * num_columns * window_size) + 10) : ((2 * num_columns * window_size) + num_signals)]),1)
     # plots  the data selection graphic
     plt.figure(1)
