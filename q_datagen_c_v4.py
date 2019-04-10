8e5649174cd758a6542b7611953257ad05bf1c7b
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-04-10 10:59:32 -0500

    Update q_datagen_c_v4.py

--- q_datagen_c_v4.py
+++ q_datagen_c_v4.py
@@ -180,7 +180,7 @@ def get_reward(action, window, min_TP, max_TP, min_SL, max_SL, min_dInv, max_dIn
                 #    reward = 1
                 #else:
                 #    reward = profit_buy / max_TP
-                reward = (profit_buy - dd_buy) / max_TP
+                reward = (profit_buy - dd_buy) / max_TP 
             # case 1: SL buy, if dir = buy, reward es el dd de buy 
             elif action == 1:
                 if dd_buy < min_SL:
