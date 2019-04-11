398d1d62f5da308e314622a68740ee4390dffaa2
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-04-10 22:25:24 -0500

    test
    test

--- q_datagen_c_v4.py
+++ q_datagen_c_v4.py
@@ -245,17 +245,14 @@ def get_reward(action, window, min_TP, max_TP, min_SL, max_SL, min_dInv, max_dIn
    # Continuous indicators = 6:rEMA, 7:rRSI, 8:rnEMA,9 rnRSI
     if action == 6:
         # RETURN DE EMA no normalizado (EMAf-EMAini) ADELANTADO 4 dias (TODO: Probar con period =7 y no 14 como el actual dataset)
-        return {'reward':(window[3][5] - window[2][5]), 'profit':0, 'dd':0 ,'min':0 ,'max':0, 'direction':0}
+        return {'reward':(window[11][5] - window[10][5]), 'profit':0, 'dd':0 ,'min':0 ,'max':0, 'direction':0}
     if action == 7:
         # RETURN DE  RSI no normalizado (RSIf - RSI ACTUAL) ADELANTADO 1 día, strat: cierra en cambio de signo de pendiente 
         return {'reward':(window[1][5] - window[0][5]), 'profit':0, 'dd':0 ,'min':0 ,'max':0, 'direction':0}
     if action == 8:
-        # RETURN DE MACD ADELANTADO 8 ticks (TODO: Probar otros valores para etrategia de prueba)
+        # RETURN DE MACD ADELANTADO 10 ticks (TODO: Probar otros valores para etrategia de prueba)
         # este tiene la menor relación balance(4219)/error(0.152)  
-        if (window[9][5] - window[8][5]) > 0:
-            rew = 1
-        else:
-            rew = 0
+        rew = (window[11][5] - window[10][5])/0.0003
         return {'reward': rew, 'profit':0, 'dd':0 ,'min':0 ,'max':0, 'direction':rew}
     if action == 9:
         # RETURN DE MACD ADELANTADO 9 ticks (TODO: Probar otros valores para etrategia de prueba)
@@ -463,9 +460,9 @@ if __name__ == '__main__':
     headers = concatenate((headers,["TPsell_"+str(min_TP)+"_"+str(max_TP)]))        
     headers = concatenate((headers,["SLsell_"+str(min_SL)+"_"+str(max_SL)]))        
     headers = concatenate((headers,["dInvsell_"+str(min_dInv)+"_"+str(max_dInv)]))         
-    headers = concatenate((headers,["rEMA4d"]))        
+    headers = concatenate((headers,["rMACD4d"]))        
     headers = concatenate((headers,["rRSI1d"]))        
-    headers = concatenate((headers,["rnEMA4d"]))        
+    headers = concatenate((headers,["rnMACD10d"]))        
     headers = concatenate((headers,["rnRSI1d"]))        
     headers = concatenate((headers,["cTPbuy_"+str(min_TP)+"_"+str(max_TP)]))        
     headers = concatenate((headers,["cSLbuy_"+str(min_SL)+"_"+str(max_SL)]))        
