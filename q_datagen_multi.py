3b7a74643edd3d971c71e415796bc9117cbe86c2
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-08-19 18:50:10 -0500

    Update q_datagen_multi.py

--- q_datagen_multi.py
+++ q_datagen_multi.py
@@ -80,9 +80,9 @@ if __name__ == '__main__':
         fig, ax = plt.subplots(figsize=(18, 7))
         current_component = mssa.components_[0, :, comp]
         cumulative_recon = cumulative_recon + current_component
-        ax.plot(s_data.index, s_data[:, 0], lw=3, alpha=0.2, c='k', label=s_data.columns[0])
-        ax.plot(s_data.index, cumulative_recon, lw=3, c='darkgoldenrod', alpha=0.6, label='cumulative'.format(comp))
-        ax.plot(s_data.index, current_component, lw=3, c='steelblue', alpha=0.8, label='component={}'.format(comp))
+        ax.plot(s_data[:, 0], lw=3, alpha=0.2, c='k', label='original')
+        ax.plot(cumulative_recon, lw=3, c='darkgoldenrod', alpha=0.6, label='cumulative'.format(comp))
+        ax.plot(current_component, lw=3, c='steelblue', alpha=0.8, label='component={}'.format(comp))
         ax.legend()
         fig.savefig('mssa_' + str(comp) + '.png', dpi=600)
         
