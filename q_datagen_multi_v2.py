0d6ca43f3d0fbeca94fa2ce8d842f473c355a59b
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-09-03 15:25:07 -0500

    Update q_datagen_multi_v2.py

--- q_datagen_multi_v2.py
+++ q_datagen_multi_v2.py
@@ -151,7 +151,7 @@ if __name__ == '__main__':
         current_component = mssa.grouped_components_[0][:, comp]
         
         cumulative_recon = cumulative_recon + current_component
-        ax.plot(s_data[:, 10], lw=3, alpha=0.2, c='k', label='original')
+        ax.plot(s_data[0:(2*p_window_size), 0], lw=3, alpha=0.2, c='k', label='original')
         ax.plot(cumulative_recon, lw=3, c='darkgoldenrod', alpha=0.6, label='cumulative'.format(comp))
         ax.plot(current_component, lw=3, c='steelblue', alpha=0.8, label='component={}'.format(comp))
         ax.legend()
