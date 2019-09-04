4ac60115231f0c4e01dea50220042a9c5015c0a5
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-09-03 23:28:30 -0500

    Update q_datagen_multi_v2.py

--- q_datagen_multi_v2.py
+++ q_datagen_multi_v2.py
@@ -102,67 +102,57 @@ if __name__ == '__main__':
             output = copy.deepcopy(mssa.components_)
         else:
             np.concatenate((output, mssa.components_), axis = 1)
+            
+        #TODO: concatenate grouped output 
+        print("Grouping correlated components (manually set list)")
+        for j in range(0, num_columns):
+            # draw correlation matrix for the first segment
+            if i == 0:
+                #total_comps = mssa.components_[j, :, :]
+                #total_wcorr = mssa.w_correlation(total_comps)
+                #total_wcorr_abs = np.abs(total_wcorr)
+                #fig, ax = plt.subplots(figsize=(12,9))
+                #sns.heatmap(np.abs(total_wcorr_abs), cmap='coolwarm', ax=ax)
+                #ax.set_title('Component w-correlations')
+                # Save the correlation matrix
+                #fig.savefig('correlation_matrix_'+str(j)+'.png', dpi=200)
+                # Set the groups based on the correlation matrix 
+                ts0_groups = [[0],[1],[2],[3],[4,5],[6],[7],[8],[9,10],[11],[12]]
+            mssa.set_ts_component_groups(j, ts0_groups)
+            ts0_grouped = mssa.grouped_components_[j]
+
+            # save the correlation matrix only for the first segment
+            #if i == 0:
+                # save grouped component correlation matrix
+                #ts0_grouped_wcor = mssa.w_correlation(ts0_grouped)
+                #fig, ax = plt.subplots(figsize=(12,9))
+                #sns.heatmap(np.abs(ts0_grouped_wcor), cmap='coolwarm', ax=ax)
+                #ax.set_title('grouped component w-correlations')
+                #fig.savefig('correlation_matrix_new_'+str(j)+'.png', dpi=200)
+        # concatenate otput array with the new components
+        if i == 0:
+            grouped_output = copy.deepcopy(mssa.grouped_components_)
+        else:
+            np.concatenate((grouped_output, mssa.grouped_components_[j]), axis = 1)
+
         # show progress
         progress = i*100/segments
         print("Segment: ",i,"/",segments, "     Progress: ", progress," %" )
 
    # Graficar matriz de correlaciones del primero y  agrupar aditivamente los mas correlated.
-    print("Grouping correlated components (manually set list)")
-    for i in range(0, rank):
-        total_comps = mssa.components_[i, :, :]
-        total_wcorr = mssa.w_correlation(total_comps)
-        total_wcorr_abs = np.abs(total_wcorr)
-        fig, ax = plt.subplots(figsize=(12,9))
-        sns.heatmap(np.abs(total_wcorr_abs), cmap='coolwarm', ax=ax)
-        ax.set_title('Component w-correlations')
-        # Save the correlation matrix
-        fig.savefig('correlation_matrix_'+str(i)+'.png', dpi=200)
-        # Set the groups based on the correlation matrix
-        #ts0_groups = [
-        #    [0]
-        #    [1,2],
-        #    [3,6],
-        #    [4,5],
-        #    [7],
-        #    [8],
-        #    [9,10],
-        #    [11,12]
-        #]
-        ts0_groups = [
-            [0],
-            [1],
-            [2],
-            [3],
-            [4,5],
-            [6],
-            [7],
-            [8],
-            [9,10],
-            [11],
-            [12]
-        ]
-        mssa.set_ts_component_groups(i, ts0_groups)
-        ts0_grouped = mssa.grouped_components_[i]
-        # save grouped component correlation matrix
-        ts0_grouped_wcor = mssa.w_correlation(ts0_grouped)
-        fig, ax = plt.subplots(figsize=(12,9))
-        sns.heatmap(np.abs(ts0_grouped_wcor), cmap='coolwarm', ax=ax)
-        ax.set_title('grouped component w-correlations')
-        fig.savefig('correlation_matrix_new_'+str(i)+'.png', dpi=200)
-    print("Original components shape: ",total_comps.shape)
-    print("Grouped components shape: ",ts0_grouped.shape)
+    print("Original components shape: ",output.shape)
+    print("Grouped components shape: ",grouped_output.shape)
     # genera gráficas para cada componente con valores agrupados
     # for the 5th and the next components, save plots containing the original and cummulative timeseries for the first data column 
     # TODO: QUITAR CUANDO DE HAGA PARA TODO SEGMENTO EN EL DATASET; NO SOLO EL PRIMERO
     cumulative_recon = np.zeros_like(s_data[:, 0])
     
     # TODO : QUITAR: TEST de tamaño de grouped_components_ dictionary
-    print("len(mssa.grouped_components_) = ", str(len(mssa.grouped_components_)))
+    #print("len(mssa.grouped_components_) = ", str(len(mssa.grouped_components_)))
     #print("mssa.grouped_components_ = ", str(mssa.grouped_components_))
-    
-    for comp in range(len(mssa.grouped_components_[0][0])):  
+    for comp in range(len(grouped_output[0][0])):  
         fig, ax = plt.subplots(figsize=(18, 7))
-        current_component = mssa.grouped_components_[0][:, comp]
+        current_component = grouped_output[0,:, comp]
         
         cumulative_recon = cumulative_recon + current_component
         ax.plot(s_data[:, 0], lw=3, alpha=0.2, c='k', label='original')
@@ -177,7 +167,7 @@ if __name__ == '__main__':
     # genera hlc+EMA(10-5,20) en formato csv para usar en qagent-test y en gym-forex separado de observaciones
     
     # TODO ERROR GUARDANDO
-    np.save(c_out_f, output)
+    np.save(c_out_f, grouped_output)
         
     # TODO: Optional:  Guardar prediction de próximos n_pred ticks por component guardados como nuevas columnas de output_buffer
     
