21d11c9e57e31f7dd49251bf32c2dcf2b318b241
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-09 00:16:40 -0500

    Update test_feature_eng.py

--- tests/test_feature_eng.py
+++ tests/test_feature_eng.py
@@ -69,7 +69,7 @@ class TestFeatureEng:
         # get the number of rows and cols from out_file
         rows_o, cols_o = self.get_size_csv(self.conf.output_file)
         # assertion
-        assert (cols_o == 1) and (rows_o == self.fe.fep.rows_d - self.fe.fep.forward_ticks)
+        assert (cols_o == 1) and (rows_o == self.fe.ep_core.rows_d - self.fe.ep_core.forward_ticks)
 
     def test_C01T03_cmdline_plugin_load(self):
         """ same as C01T02, but via command-line """
@@ -78,12 +78,12 @@ class TestFeatureEng:
             + " --output_file "
             + self.conf.output_file
             + " --forward_ticks "
-            + str(self.fe.ep_core.forward_ticks)
+            + str(self.conf.forward_ticks)
         )
         # get the size of the output dataset
         rows_d, cols_d = self.get_size_csv(self.conf.input_file)
         # get the size of the output dataset
         rows_o, cols_o = self.get_size_csv(self.conf.output_file)
         # assert if the number of rows an colums is less than the input dataset and > 0
-        assert (cols_o == 1) and (rows_o == rows_d - self.fe.ep_core.forward_ticks)
+        assert (cols_o == 1) and (rows_o == rows_d - self.conf.forward_ticks)
         
