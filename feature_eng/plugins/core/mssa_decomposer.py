e14b209749d3fe9a9618d5b70b439ced45e94ff2
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-25 14:59:35 -0500

    Update mssa_decomposer.py

--- feature_eng/plugins/core/mssa_decomposer.py
+++ feature_eng/plugins/core/mssa_decomposer.py
@@ -83,7 +83,7 @@ class MSSADecomposer(PluginBase):
                 #TODO: concatenate grouped output
                 print("Grouping correlated components (manually set list)")
                 # TODO: QUITAR GUARDADO DE JSON DE EJEMPLO
-                ts0_groups = [[0],[1],[2],[3],[4,5],[6],[7],[8],[9,10],[11],[12]]
+                ts0_groups = [[0],[1],[2,3]]
                 with open(self.conf.group_file, 'w') as f:
                     json.dump(ts0_groups, f)
                 with open(self.conf.group_file) as json_file:
