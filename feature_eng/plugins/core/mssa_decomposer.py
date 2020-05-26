066f6dadebb7e0e1ee6e869fa4aaee28b2433892
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-26 15:41:39 -0500

    Update mssa_decomposer.py

--- feature_eng/plugins/core/mssa_decomposer.py
+++ feature_eng/plugins/core/mssa_decomposer.py
@@ -42,8 +42,9 @@ class MSSADecomposer(PluginBase):
         except:
             (self.rows_d,) = input_ds.shape
             self.cols_d = 1
+            input_ds.reshape(self.rows_d,self.cols_d)
         # create an empty array with the estimated output shape
-        print("self.rows_d = ", self.rows_d)
+
         self.output_ds = np.empty(shape=(self.rows_d-self.conf.window_size, 1))
         # calculate the output by performing MSSA on <segments> number of windows of data of size window_size
         segments = (self.rows_d // (2*self.conf.window_size))
