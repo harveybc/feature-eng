5e3e87809df022eeb5fe8d9e8046da5564cda943
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-26 14:05:31 -0500

    Update test_mssa_decomposer.py

--- tests/test_mssa_decomposer.py
+++ tests/test_mssa_decomposer.py
@@ -22,7 +22,7 @@ class Conf:
         """ Output dataset filename """
         self.list_plugins = False
         self.core_plugin = "mssa_decomposer"
-        self.num_components = 0
+        self.num_components = 7
         self.window_size = 30
         self.group_file = None
         self.plot_prefix = None
@@ -145,6 +145,24 @@ class TestMSSADecomposer:
             + self.conf.output_file
             + " --num_components "
             + str(self.conf.num_components)
+            + " --plot_prefix "
+            + os.path.join(os.path.dirname(__file__), "data/plot_")
+        ) 
+        # get the size of the output dataset
+        rows_d, cols_d = self.get_size_csv(self.conf.input_file)
+        # get the size of the output dataset
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assert if there are 3 groups per feature in the output dataset
+        #TODO: ASSERT IF PLOT FILE EXISTS
+        assert (cols_o == self.cols_d * self.conf.num_components)
+    
+    def test_C03T07_svht_plot_prefix(self):
+        """ assert if there are 3 groups per feature in the output dataset """
+        os.system("feature_eng --core_plugin mssa_decomposer --input_file "
+            + self.conf.input_file
+            + " --output_file "
+            + self.conf.output_file
+            + " --num_components 0"
             + " --plot_prefix "
             + os.path.join(os.path.dirname(__file__), "data/plot_")
         ) 
