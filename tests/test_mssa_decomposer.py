9690afb5cc2567f6cad7de225f5a73966947a41e
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-26 00:11:50 -0500

    Update test_mssa_decomposer.py

--- tests/test_mssa_decomposer.py
+++ tests/test_mssa_decomposer.py
@@ -113,7 +113,7 @@ class TestMSSADecomposer:
         # get the size of the output dataset
         rows_o, cols_o = self.get_size_csv(self.conf.output_file)
         # assert if there are 3 groups per feature in the output dataset
-        assert (cols_o == self.cols_d * self.conf.num_component)
+        assert (cols_o == self.cols_d * self.conf.num_components)
     
     def test_C03T05_w_prefix_group_file(self):
         """ assert if there are 3 groups per feature in the output dataset """
@@ -133,4 +133,4 @@ class TestMSSADecomposer:
         # get the size of the output dataset
         rows_o, cols_o = self.get_size_csv(self.conf.output_file)
         # assert if there are 3 groups per feature in the output dataset
-        assert (cols_o == self.cols_d * self.conf.num_components)
+        assert (cols_o == self.cols_d * 3)
