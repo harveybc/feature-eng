68175b3b4231b79ed765d48411f5980bc4a534e7
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-05-26 19:44:53 -0500

    added mssa_predictor readme

--- README_mssa_predictor.md
+++ README_mssa_predictor.md
@@ -1,6 +1,6 @@
-# FeatureEng: MSSA-Decomposer
+# FeatureEng: MSSA-Predictor
 
-Performs Multivariate Singular Spectrum Analysis (MSSA) decomposition of an input dataset.
+Performs sliding window, tick by tick Multivariate Singular Spectrum Analysis (MSSA) of an input dataset and make a prediction of a configurable number of ticks forward for each feature of the input dataset.
 
 [![Build Status](https://travis-ci.org/harveybc/feature_eng.svg?branch=master)](https://travis-ci.org/harveybc/feature_eng)
 [![Documentation Status](https://readthedocs.org/projects/docs/badge/?version=latest)](https://harveybc-feature_eng.readthedocs.io/en/latest/)
@@ -9,8 +9,9 @@ Performs Multivariate Singular Spectrum Analysis (MSSA) decomposition of an inpu
 
 ## Description
 
-Performs MSSA decomposition of an input dataset, uses a configurable number of output channels, optionally 
-grouping similar components.
+Performs MSSA decomposition of an input dataset, uses a configurable number of output channels and a configurable window size, generate a prediction for each input feature and the output dataset has num_rows = input_num_rows-(window_size+forward_ticks).
+
+Optionally, this plugin calculate the error (MSE) between the input dataset and the output dataset. 
 
 ## Installation
 
@@ -19,7 +20,7 @@ The plugin is pre-installed with the feature_eng package, the instructions are d
 ### Command-Line Execution
 
 The plugin's core method can be executed by loading the plugin by instantiating a FeatureEng class with plugin-specific configuration parameters and also, it can be used from the console command feature_eng, available after installing the [feature_eng package](../master/README.md):
-> feature_eng --core_plugin mssa_decomposer --input_file <input_dataset> <optional_parameters>
+> feature_eng --core_plugin mssa_predictor --input_file <input_dataset> <optional_parameters>
 
 ### Command-Line Parameters
 
@@ -27,10 +28,8 @@ The plugin's core method can be executed by loading the plugin by instantiating 
 * __--output_file <filename>__: (Optional) Filename for the output dataset for the default feature_eng output plugin (store_csv). Defaults to output.csv
 * __--num_components <val>__:(Optional) Number of SSA components per input feature. Defaults to 0 = Autocalculated usign Singular Value Hard Thresholding (SVHT).
 * __--window_size <val>__: (Optional) Size of the window used for analysis. Defaults to 30.
-* __--group_file <filename>__: (Optional) Filename for the JSON file containing manually set feature groups. Use --plot_correlation to export a w-correlation matrix plot. Defaults to None.
-* __--w_prefix <filename_prefix>__: (Optional) Exports plots of the w-correlation matrix for grouped components for each feature. Defaults to None.
-* __--plots_prefix <filename_prefix>__: (Optional) Exports plots of each grouped channel superposed to the input dataset. Defaults to None.
-
+* __--plots_prefix <filename_prefix>__: (Optional) Exports a plot of the prediction superposed to the input dataset. Defaults to None.
+* __--show_error__: (Optional) Calculate the Mean Squared Error (MSE) between the prediction and the input future value. Defaults to False.
 
 ## Examples of usage
 
@@ -41,7 +40,7 @@ from feature_eng.feature_eng import FeatureEng
 # configure parameters (same variable names as command-line parameters)
 class Conf:
     def __init__(self):
-        self.core_plugin = "mssa_decomposer"
+        self.core_plugin = "mssa_predictor"
         self.input_file = "tests/data/test_input.csv"
 # initialize instance of the Conf configuration class
 conf = Conf()
---
+++ tests/test_mssa_predictor.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import csv
+import sys
+import os
+from filecmp import cmp
+from feature_eng.feature_eng import FeatureEng
+
+__author__ = "Harvey Bastidas"
+__copyright__ = "Harvey Bastidas"
+__license__ = "mit"
+
+class Conf:
+    """ This method initialize the configuration variables for a plugin """
+    
+    def __init__(self):
+        """ Component Tests Constructor """
+        self.input_file = os.path.join(os.path.dirname(__file__), "data/test_c02_output.csv")
+        """ Test dataset filename """
+        self.output_file = os.path.join(os.path.dirname(__file__), "data/test_c03_output.csv")
+        """ Output dataset filename """
+        self.list_plugins = False
+        self.core_plugin = "mssa_predictor"
+        self.num_components = 8
+        self.window_size = 30
+        self.plot_prefix = None
+        self.forward_ticks = 5
+
+class TestMSSADecomposer:
+    """ Component Tests  """
+
+    def setup_method(self, test_method):
+        """ Component Tests Constructor """
+        self.conf = Conf()
+        self.rows_d, self.cols_d = self.get_size_csv(self.conf.input_file)
+        """ Get the number of rows and columns of the test dataset """
+        try:
+            os.remove(self.conf.output_file)
+        except:
+            print("No test output file found.")
+            pass
+
+    def get_size_csv(self, csv_file):
+        """ Get the number of rows and columns of a test dataset, used in all tests.
+
+        Args:
+        csv_file (string): Path and filename of a test dataset
+
+        Returns:
+        (int,int): number of rows, number of columns
+        """
+        rows = list(csv.reader(open(csv_file)))
+        return (len(rows), len(rows[0]))
+
+
+    def test_C04T01_core(self):
+        """ Loads plugin from FeatureEng using parameters from setup_method() and Asses that output file has same number of columns but less rows  """
+        self.fe = FeatureEng(self.conf)
+        # get the number of rows and cols from out_file
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assertion
+        assert (cols_o == self.conf.cols_d) and (rows_o == self.conf.rows_d-(self.conf.window_size+self.conf.forward_ticks))
+
+    def test_C04T02_cmdline(self):
+        """ same as C04T01, but via command-line """
+        os.system("feature_eng --core_plugin mssa_predictor --input_file "
+            + self.conf.input_file
+            + " --output_file "
+            + self.conf.output_file
+            + " --num_components "
+            + str(self.conf.num_components)
+        )
+        # get the size of the output dataset
+        rows_d, cols_d = self.get_size_csv(self.conf.input_file)
+        # get the size of the output dataset
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assertion
+        assert (cols_o == self.conf.cols_d) and (rows_o == self.conf.rows_d-(self.conf.window_size+self.conf.forward_ticks))
+
+    def test_C04T03_plot_prefix(self):
+        """  """
+        os.system("feature_eng --core_plugin mssa_predictor --input_file "
+            + self.conf.input_file
+            + " --output_file "
+            + self.conf.output_file
+            + " --num_components "
+            + str(self.conf.num_components)
+            + " --plot_prefix "
+            + os.path.join(os.path.dirname(__file__), "plots/")
+        ) 
+        # get the size of the output dataset
+        rows_d, cols_d = self.get_size_csv(self.conf.input_file)
+        # get the size of the output dataset
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assert if there are 3 groups per feature in the output dataset
+        #TODO: ASSERT IF PLOT FILE EXISTS
+        # assertion
+        assert (cols_o == self.conf.cols_d) and (rows_o == self.conf.rows_d-(self.conf.window_size+self.conf.forward_ticks))
+    
+    def test_C04T04_svht_plot_prefix(self):
+        """  """
+        os.system("feature_eng --core_plugin mssa_predictor --input_file "
+            + self.conf.input_file
+            + " --output_file "
+            + self.conf.output_file
+            + " --num_components 0"
+            + " --plot_prefix "
+            + os.path.join(os.path.dirname(__file__), "plots/svht_")
+        ) 
+        # get the size of the output dataset
+        rows_d, cols_d = self.get_size_csv(self.conf.input_file)
+        # get the size of the output dataset
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assert if there are 3 groups per feature in the output dataset
+        #TODO: ASSERT IF PLOT FILE EXISTS
+        assert (cols_o > self.cols_d)
+
+    def test_C04T05_svht_plot_prefix_show_error(self):
+        """  """
+        os.system("feature_eng --core_plugin mssa_predictor --input_file "
+            + self.conf.input_file
+            + " --output_file "
+            + self.conf.output_file
+            + " --num_components 0"
+            + " --plot_prefix "
+            + os.path.join(os.path.dirname(__file__), "plots/svht_")
+            + " --show_error "
+        ) 
+        # get the size of the output dataset
+        rows_d, cols_d = self.get_size_csv(self.conf.input_file)
+        # get the size of the output dataset
+        rows_o, cols_o = self.get_size_csv(self.conf.output_file)
+        # assert if there are 3 groups per feature in the output dataset
+        #TODO: ASSERT IF PLOT FILE EXISTS
+        assert (cols_o > self.cols_d)
