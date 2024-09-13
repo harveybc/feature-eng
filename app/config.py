9b2b9d1af29e3f4646279fcf19d983e40de53fbd
Harvey Bastidas <harveybc@ingeni-us.com>
2024-09-12 23:52:07 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,13 +7,13 @@ DEFAULT_VALUES = {
     'correlation_analysis': False,
     'distribution_plot': False,
     'quiet_mode': False,
-    'save_log': './logs/debug_log.json',
+    'save_log': './debug_log.json',
     'username': None,
     'password': None,
     'remote_save_config': None,
     'remote_load_config': None,
     'remote_log': None,
-    'load_config': './config/input_config.json',
-    'save_config': './config/output_config.json',
+    'load_config': None,
+    'save_config': './output_config.json',
     'headers': True
 }
